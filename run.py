49e087ba27fe1c5b959a7cd50345be19e47a7145
Nauvalunesa <24031554092@mhs.unesa.ac.id>
2025-08-08 20:21:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
+import subprocess
 import uvicorn
 from main import app
 
 if __name__ == "__main__":
+    
+    subprocess.run(["playwright", "install", "chromium"], check=True)
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=7860,  
+        port=7860,
         proxy_headers=True,
         forwarded_allow_ips="*"
     )
+
 
   
